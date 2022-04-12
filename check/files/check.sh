ccba5f057bdf6fdb2554ba6064bd1dc4426431fe
ShipengHu-777 <35558007+ShipengHu-777@users.noreply.github.com>
2022-04-12 16:33:18 +0800

    Add files via upload

---
+++ check/files/check.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#check open files and max user process
+ulimit  -a |egrep 'open files|max user processes'|awk '{ if($1$2 == "openfiles") { if(  $4 <  64000 ) print "Warning: now open files is "$4", and need to turn up to 64000";} if( $1$2$3 == "maxuserprocesses" ) { if(  $5 < 65535 ) print "Warning: now max user processes is "$5", and need to turn up to 65535";} }'
+
+#check swap
+mem=$(free -g|grep 'Mem:'|awk '{ print $2; }' )
+free -g|grep 'Swap'|awk '{ if( int($2) < '"$(( 128-$mem))"' ) print "Warning: now swap is "$2", need to turn up"; }'
+
+#check fate process
+pnum=$( ps aux|egrep -v "grep|fate/tools/check.sh|serving|ansilbe|tail|" | grep -c fate  );
+if [ $pnum -gt 0 ]
+then
+  echo "Warning: key fate process exists, please has a check and clean"
+fi
+
+#check port
+open_ports=( $( /usr/sbin/ss -lnt|grep 'LISTEN'|grep -v ':::' | awk '{print ; }'|cut -d : -f 2|sort -u ) )
+tports=()
+for oport in ${open_ports[*]};
+do
+  for port in "9370" "4670" "4671" "9360" "9380" "3306";
+  do
+    if [ $oport == $port ]
+    then
+      tports=( ${tports[*]} $port )
+    fi
+  done
+done
+if [ ${#tports[*]} -gt 0 ]
+then
+  echo "Warning: these ports: ${tports[*]} have been used"
+fi
+
+#check mysql
+if [ -f /etc/my.cnf ]
+then
+  echo "Warning: if reinstall mysql, please stop mysql, and rename /etc/my.cnf"
+fi
+
+#check dircetory
+if [ -d "/data/projects/fate/common" || -d "/data/projects/fate/eggroll" || -d "/data/projects/fate/fateboard" || -d "/data/projects/fate/python" || -d "/data/projects/fate/example" ]
+then
+  echo "Warning: please rename /data/projects/fate/{common|eggroll|fateboard|python|example}"
+fi
+
+if [ -d "/data/projects/data/fate/mysql" ]
+then
+  echo "Warning: please rename /data/projects/data/fate/mysql"
+fi
+
+num=$( ls /data/projects/common/supervisord/supervisord.d/fate-*.conf | wc -l )
+if [ $num -gt 0 ]
+then
+  echo "Warning: supervisor_fate_conf exists, please remove /data/projects/common/supervisord/supervisord.d/fate-*.conf and update all"
+fi
+
